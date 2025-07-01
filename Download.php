6d7cbe3cb68bcfdf425188636952e44c2fc12948
hkgjdh <eonhemant20@gmail.com>
2025-07-01 14:36:07 +0530

    Update Download.php

--- Download.php
+++ Download.php
@@ -1,4 +1,4 @@
 {
   "version": "0.01",
-  "url": "https://raw.githubusercontent.com/hkgjdh/HK-CHEATS-LOADER-V2/main/libHKCHEATS.so"
+  "url": "https://raw.githubusercontent.com/hkgjdh/NINJA-CHEAT-VIP/main/libHKCHEATS.so"
 }
